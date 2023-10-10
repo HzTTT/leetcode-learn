956b6adafb87cb9824af232382068e2a0b638414
hongzaitou <767067749@qq.com>
2023-10-10 15:11:32 +0800

    数组：双指针去重-fixbug

--- 数组/26.删除有序数组中的重复项.py
+++ 数组/26.删除有序数组中的重复项.py
@@ -12,12 +12,11 @@ class Solution:
             return 0
         
         slow = 0  # 慢指针
+        i = len(nums)
         for fast in range(len(nums)):  # 快指针遍历整个列表
             if nums[fast] != nums[slow]:  # 当快指针指向的元素与慢指针指向的元素不相等时
                 slow += 1  # 慢指针向前移动一步
                 nums[slow] = nums[fast]  # 更新慢指针指向的元素为快指针指向的元素
-            fast += 1  # 快指针向前移动一步
-        
         return slow + 1  # 返回去重后的列表长度
 # @lc code=end
 
